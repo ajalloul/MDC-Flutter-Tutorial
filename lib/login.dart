2e3dd6adf5b032b5894d5375da93c7f7afa6ea51
adnanjalloul <adnan.jalloul96@gmail.com>
2020-02-24 00:37:33 +0200

    Added snackbar and field check to login page.

--- lib/login.dart
+++ lib/login.dart
@@ -23,64 +23,81 @@ class _LoginPageState extends State<LoginPage> {
   // TODO: Add text editing controllers (101)
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          children: <Widget>[
-            SizedBox(height: 80.0),
-            Column(
-              children: <Widget>[
-                Image.asset('assets/diamond.png'),
-                SizedBox(height: 16.0),
-                Text('SHRINE'),
-              ],
-            ),
-            SizedBox(height: 120.0,
-            width: 150.0),
-            // TODO: Wrap Username with AccentColorOverride (103)
-            // TODO: Remove filled: true values (103)
-            // TODO: Wrap Password with AccentColorOverride (103)
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                filled: true,
-                labelText: 'Username',
+      body: Builder(
+        builder: (context) => Center(
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            children: <Widget>[
+              SizedBox(height: 80.0),
+              Column(
+                children: <Widget>[
+                  Image.asset('assets/diamond.png'),
+                  SizedBox(height: 16.0),
+                  Text('SHRINE'),
+                ],
               ),
-            ),
-            SizedBox(height: 12.0),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                filled: true,
-                labelText: 'Password'
-              ),
-                obscureText: true,
-            ),
-            ButtonBar(
-              children: <Widget>[
-                FlatButton(
-                  child: Text('Cancel'),
-                  onPressed: (){
-                    _usernameController.clear();
-                    _passwordController.clear();
-                  },
+              SizedBox(height: 120.0, width: 150.0),
+              // TODO: Wrap Username with AccentColorOverride (103)
+              // TODO: Remove filled: true values (103)
+              // TODO: Wrap Password with AccentColorOverride (103)
+              TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  filled: true,
+                  labelText: 'Username',
                 ),
-                RaisedButton(
-                  child: Text('Next'),
-                  onPressed: (){
-                    if(_usernameController.text.isNotEmpty && _passwordController.text.isNotEmpty)
-                      Navigator.pop(context);
-                  },
-                )
-              ],
-            )
-          ],
+              ),
+              SizedBox(height: 12.0),
+              TextField(
+                controller: _passwordController,
+                decoration:
+                    InputDecoration(filled: true, labelText: 'Password'),
+                obscureText: true,
+              ),
+              ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: Text('Cancel'),
+                    onPressed: () {
+                      _usernameController.clear();
+                      _passwordController.clear();
+                    },
+                  ),
+                  RaisedButton(
+                    child: Text('Next'),
+                    onPressed: () {
+                      login(context);
+                    },
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void login(BuildContext context) {
+    if (_usernameController.text.isNotEmpty &&
+        _passwordController.text.isNotEmpty)
+      Navigator.pop(context);
+    else {
+      final snackbar = SnackBar(
+        content: Text('Please enter a username & password'),
+        action: (SnackBarAction(
+          label: 'Retry',
+          onPressed: (){
+            login(context);
+          },
+        )),
+      );
+      Scaffold.of(context).showSnackBar(snackbar);
+    }
   }
 }
 
